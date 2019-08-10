d23729570a761790d3e296ce88fbda6071c7a876
宇井洋樹 <ui@rehabforjapan.com>
2019-08-10 23:39:23 +0900

    favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  root to: 'condition#information'
   get 'condition/information' => 'condition/information'
   get 'condition/calc' => 'condition/calc'
 
